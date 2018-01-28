1121ed81c18a71633ab612482c14242cf44e5d69
Jatawatsafe <jatawat.safe@mail.kmutt.ac.th>
2018-01-28 17:24:08 +0700

    [ADD] reverseArray Method

--- INT105/src/Array/CalculateSumPartOfNum.java
+++ INT105/src/Array/CalculateSumPartOfNum.java
@@ -4,6 +4,11 @@ public class CalculateSumPartOfNum {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int[] num = new int[]{12,85,96,78,12};
+        System.out.print("Reverse: ");
+        for(int i=0;i<num.length;i++){
+            System.out.print(reverseArray(num)[i]+" ");
+        }
+        System.out.println("\n");
         System.out.print("Enter First index: ");
         int firstIndex = input.nextInt();
         System.out.print("Enter Last index: ");
@@ -17,5 +22,13 @@ public class CalculateSumPartOfNum {
             sum=sum+num[i];
         }
         return sum;
+    }
+    
+    public static int[] reverseArray (int[] num){
+        int[] reverseNum = new int[num.length];
+        for(int i=0;i<num.length;i++){
+            reverseNum[i]=num[num.length-(1+i)];
+        }
+        return reverseNum;            
     }
 }
