cafbdac7451b49be460d4ef1f737ef4a0b6be616
INT105 <INT105@10.4.21.111>
2018-01-29 11:48:01 +0700

    [Upload] file: TestDeck (forDeckTest)

---
+++ Chapter2/src/student/view/TestDeckCard.java
@@ -0,0 +1,11 @@
+package student.view;
+import student.model.Deck;
+public class TestDeckCard {
+    public static void main(String[] args) {
+        Deck deck = new Deck();     //Deck หนึ่งสำรับแบบปกติ
+        System.out.println(deck);
+        
+        
+        
+    }
+}
