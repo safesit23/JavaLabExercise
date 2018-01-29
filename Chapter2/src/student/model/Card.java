ea03612074611064f5e35913e5d819ed8690dd05
INT105 <INT105@10.4.21.111>
2018-01-29 11:13:23 +0700

    [Upload] file: Card (forDeckTest)

---
+++ Chapter2/src/student/model/Card.java
@@ -0,0 +1,31 @@
+package student.model;
+public class Card {
+    private String suit;
+    private int rank;
+
+    public Card(String suit, int rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" + "suit=" + suit + ", rank=" + rank + '}';
+    }
+}
