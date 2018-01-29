fab685a45795522becb85fa4483d71677e74100a
INT105 <INT105@10.4.21.111>
2018-01-29 11:59:07 +0700

    [ADD] comment Deck (forDeckTest)

--- Chapter2/src/student/model/Deck.java
+++ Chapter2/src/student/model/Deck.java
@@ -1,29 +1,27 @@
 package student.model;
 public class Deck {
-    private Card[] cards;
+    private Card[] cards;           //ดึงการ์ดมาใช้งาน
 
-    public Deck(Card[] Cards) {
+    public Deck(Card[] Cards) {     //ในกรณีสร้างการ์ดเองใน TestDeckCard
         this.cards = Cards;
     }
 
-    public Deck() {
-        this.cards = new Card[52];
-        int j=0;
-        for(int i=1;i<=13;i++){
-            String cardName="";
+    public Deck() {                 //ในกรณี new Deck ใน TestDeckCard
+        this.cards = new Card[52];  //สร้างจำนวนการ์ด 52 ใบแบบ Array
+        int j=0;                    //
+        for(int i=1;i<=13;i++){     //วน LOOP เพื่อกำหนดชื่อการ์ด 1-13
+            String cardName="";     
             switch(i){
-                case 11:    cardName= "Jack";   break;
-                case 12:    cardName= "Queen";  break;
-                case 13:    cardName= "King";   break;
-                default:    cardName= ""+i; 
+                case 11:    cardName= "Jack";   break;  //ถ้าเป็นเลข 11 เป็น Jack
+                case 12:    cardName= "Queen";  break;  //ถ้าเป็นเลข 12 เป็น Queen
+                case 13:    cardName= "King";   break;  //ถ้าเป็นเลข 13 เป็น King
+                default:    cardName= ""+i;             //เลข 1-10 เป็นตัวเลขปกติ
             }
-            cards[j++] = new Card(cardName, "Club");
-            cards[j++] = new Card(cardName, "Diamonds");
-            cards[j++] = new Card(cardName, "Heart");
-            cards[j++] = new Card(cardName, "Spade");
+            cards[j++] = new Card(cardName, "Club");        //ใบที่ 1. เลขเดิม ให้เป็นประเภท Club
+            cards[j++] = new Card(cardName, "Diamonds");    //ใบที่ 2. เลขเดิม ให้เป็นประเภท Diamonds
+            cards[j++] = new Card(cardName, "Heart");       //ใบที่ 3. เลขเดิม ให้เป็นประเภท Heart
+            cards[j++] = new Card(cardName, "Spade");       //ใบที่ 4. เลขเดิม ให้เป็นประเภท Spade
         }
-        
-        
     }
 
     public Card[] getCards() {
@@ -37,8 +35,8 @@ public class Deck {
     @Override
     public String toString() {
         String showCard = "";
-        for(Card card: cards){
-            showCard += card +"\n";
+        for(Card card: cards){          //โชว์ข้อมูลของการ์ดทั้ง 52 ใบโดยใช้ LOOP
+            showCard += card +"\n";     
         }
         return showCard;
     }
