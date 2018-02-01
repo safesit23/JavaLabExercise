e5e62a48bf125927bf6cd8b39e947698878ff830
Jatawatsafe <jatawat.safe@mail.kmutt.ac.th>
2018-02-02 00:03:45 +0700

    [Upload] JavaOverflowQ1

---
+++ INT105/src/Platform/JavaOverflow.java
@@ -0,0 +1,12 @@
+package Platform;
+public class JavaOverflow {
+    public static void main(String[] args) {
+        //Question1: Print the minimum value (MIN_VALUE) and maximum value (MAX_VALUE) of Byte.
+        byte min=Byte.MIN_VALUE;
+        byte max=Byte.MAX_VALUE;
+        System.out.println("Question1: ");
+        System.out.println("Minimum Value: "+min);
+        System.out.println("Maximum Value: "+max);
+        
+    }
+}
