0f92be4257aac68fddcc6a017d3aad25f103f058
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-02-02 12:04:16 +0700

    [Upload] FindMinMax

---
+++ MacProINT105/src/macproint105/FindMinMax.java
@@ -0,0 +1,13 @@
+package macproint105;
+public class FindMinMax {
+    public static void main(String[] args) {
+        int[] score={100,30,60,20,80};
+        System.out.println(findMinMax(score));
+    }
+    public static int[] findMinMax(int[] score){
+        int[] result=null;
+        
+        return result;
+    }
+    
+}
