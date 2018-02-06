eb98481b52fe1ed0f2472755ff114701e80c32e3
Jatawatsafe <jatawat.safe@mail.kmutt.ac.th>
2018-02-06 21:44:59 +0700

    [Add] Q5 Check variables is Nan or not

--- INT105/src/Platform/FloatingPoint.java
+++ INT105/src/Platform/FloatingPoint.java
@@ -28,9 +28,13 @@ public class FloatingPoint {
         float divFloat1=(float)1/0;
         float divFloat2=(float)0/0;
         float sqrtFloat=(float) Math.sqrt(-1);
-        System.out.println("Value of divFloat1 = "+divFloat1);
-        System.out.println("Value of divFloat2 = "+divFloat2);
-        System.out.println("Value of sqrtFloat = "+sqrtFloat);
+        System.out.println("2.Value of divFloat1 = "+divFloat1);
+        System.out.println("3.Value of divFloat2 = "+divFloat2);
+        System.out.println("4.Value of sqrtFloat = "+sqrtFloat);
+        System.out.println("5.Variables\tis NaN");
+        System.out.println("divFloat1:\t"+(divFloat1 != divFloat1)+"\t"+(Float.isNaN(divFloat1)));
+        System.out.println("divFloat2:\t"+(divFloat2 != divFloat2)+"\t"+(Float.isNaN(divFloat2)));
+        System.out.println("sqrtFloat:\t"+(sqrtFloat != sqrtFloat)+"\t"+(Float.isNaN(sqrtFloat)));
         
         
     }
