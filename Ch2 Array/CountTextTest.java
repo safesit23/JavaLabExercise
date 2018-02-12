c4c7d17d136bed4a83935b889d938277430710f6
Jatawatsafe <35445358+safesit23@users.noreply.github.com>
2018-02-12 15:49:20 +0700

    [Add] Test Case

--- Ch2 Array/CountTextTest.java
+++ Ch2 Array/CountTextTest.java
@@ -1,17 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import model.CountText;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Tisanai.Cha
- */
 public class CountTextTest {
 
     private String sentence = "Java Application";
