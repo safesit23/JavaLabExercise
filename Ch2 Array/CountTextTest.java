7f4f662195e4e4a31bb0a12c7e6e0fd67cac08a7
Jatawatsafe <35445358+safesit23@users.noreply.github.com>
2018-02-12 15:47:55 +0700

    [Upload] CountText

---
+++ Ch2 Array/CountTextTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import model.CountText;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Tisanai.Cha
+ */
+public class CountTextTest {
+
+    private String sentence = "Java Application";
+    private char[] char4count = {'a', 'I', 'c'};
+    
+    private String sentence2 = "Hello WorLd";
+    private char[] char4count2 = {'H', 'e', 'l', 'o', 'W'};
+    
+
+    @Test
+    public void checkResult() {
+        int[] expectResult = {4, 2, 1};
+        int[] result = CountText.countCharInText(sentence, char4count);
+        assertArrayEquals(expectResult, result);
+    }
+
+    @Test
+    public void checkLength() {
+        int expectResult = 3;
+        int result = CountText.countCharInText(sentence, char4count).length;
+        assertEquals(expectResult, result);
+    }
+    
+    @Test
+    public void checkResult2() {
+        int[] expectResult = {1, 1, 3, 2, 1};
+        int[] result = CountText.countCharInText(sentence2, char4count2);
+        assertArrayEquals(expectResult, result);
+    }
+
+    @Test
+    public void checkLength2() {
+        int expectResult = 5;
+        int result = CountText.countCharInText(sentence2, char4count2).length;
+        assertEquals(expectResult, result);
+    }
+}
