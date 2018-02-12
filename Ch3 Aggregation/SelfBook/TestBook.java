72b216e12d1b1fbf4d12060b47e228f5036b0db2
Jatawatsafe <35445358+safesit23@users.noreply.github.com>
2018-02-12 11:17:39 +0700

    [Add] Equal

--- Ch3 Aggregation/SelfBook/TestBook.java
+++ Ch3 Aggregation/SelfBook/TestBook.java
@@ -3,8 +3,17 @@ public class TestBook {
     public static void main(String[] args) {
         BookCatalog bc = new BookCatalog(2);
         Book b1 = new Book(123456789, "Web Programming", "programming", true);
+        System.out.println("Index that add book: "+bc.addBook(b1));
+        System.out.println("NumOfBook: "+bc.getNumOfBooks());
         
-        System.out.println(bc.addBook(b1));
+        //Test Equal Method
+        if(b1.equals("hghghghg")){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+        
+        System.out.println("PRINT BOOK");
         for(int i=0;i<bc.getBooks().length;i++){
            System.out.println(bc.getBooks()[i]);
         }
