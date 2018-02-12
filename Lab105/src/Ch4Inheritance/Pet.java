175afb605a935c123b74ff2cec6e234893e0d972
INT105 <INT105@10.4.21.111>
2018-02-12 08:44:15 +0700

    [Upload] Animal and Pet

---
+++ Lab105/src/Ch4Inheritance/Pet.java
@@ -0,0 +1,18 @@
+package Ch4Inheritance;
+public class Pet extends Animal{
+    private String ownerName;
+
+    public Pet(String ownerName, String name) {
+        super(name);
+        this.ownerName = ownerName;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+    
+}
