d6d504a8eda08000d6f28cf76a34b79a8216360e
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-02-14 14:35:08 +0700

    [Upload] Hero

--- MacProINT105/src/macproint105/Hero.java
+++ MacProINT105/src/macproint105/Hero.java
@@ -1,4 +1,19 @@
 package macproint105;
 public class Hero {
+    private String name;
+    private int health;
+    private Weapon weapon;
+    private Armor armor;
+
+    public Hero(String name, int health, Weapon weapon, Armor armor) {
+        this.name = name;
+        this.health = health;
+        this.weapon = weapon;
+        this.armor = armor;
+    }
+
+    public int getHealth() {
+        return health;
+    }
     
 }
