31d329567f280d4e2f89ac8f6a6ee6c7c72c0394
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-02-14 14:35:13 +0700

    [Upload] Hero

---
+++ MacProINT105/src/macproint105/Weapon.java
@@ -0,0 +1,21 @@
+package macproint105;
+public class Weapon {
+    private String name;
+    private int power;
+
+    public Weapon(String name, int power) {
+        this.name = name;
+        this.power = power;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    @Override
+    public String toString() {
+        return "Weapon{" + "name=" + name + ", power=" + power + '}';
+    }
+    
+    
+}
