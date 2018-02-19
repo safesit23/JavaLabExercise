bbbf1d1be86c6252853df43b550b1835cbfe0ec1
INT105 <INT105@10.4.21.111>
2018-02-19 09:32:00 +0700

    [Upload] Shape

---
+++ Ch5 Polymorphism/src/Shape/Circle2.java
@@ -0,0 +1,18 @@
+package Shape;
+public class Circle2 extends Circle{
+
+    public Circle2(double radius, int shaped) {
+        super(radius, shaped);
+    }
+    
+    
+    public double findCircumference(){
+        return 2*Math.PI*getRadius();    //ใช้ Super. ไม่ได้เพราะไม่ได้ Shadow กัน *ห้ามแก้ private ที่พ่อ
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+    
+}
