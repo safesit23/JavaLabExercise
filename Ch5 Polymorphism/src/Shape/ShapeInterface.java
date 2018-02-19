e70cbc5936159e1a5615af6bb9b3405c4977df16
INT105 <INT105@10.4.21.111>
2018-02-19 10:20:37 +0700

    [Upload] ShapeInterface

---
+++ Ch5 Polymorphism/src/Shape/ShapeInterface.java
@@ -0,0 +1,8 @@
+package Shape;
+public interface ShapeInterface {
+    int COLOR=256;                  //public final static COLOR=256;
+    double findArea();              //public abstract double findArea();
+    double findCircumference();
+    
+}
+
