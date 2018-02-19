8e35224867c164c794817d29545a91b9bfd7f39b
INT105 <INT105@10.4.21.111>
2018-02-19 10:20:41 +0700

    [Upload] ShapeInterface

---
+++ Ch5 Polymorphism/src/Shape/SquareInterface.java
@@ -0,0 +1,18 @@
+package Shape;
+public abstract class SquareInterface implements ShapeInterface{
+    private double side;
+
+    public SquareInterface(double side) {
+        this.side = side;
+    }
+    
+    
+    public double getSide() {
+        return side;
+    }
+    
+    public double findArea(){
+        return side*side;
+    }
+}
+
