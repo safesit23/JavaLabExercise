5c556db5d1e17acb8b3237f88c7beadd94af0664
INT105 <INT105@10.4.21.111>
2018-02-19 10:12:00 +0700

    [Upload] Shape

--- Ch5 Polymorphism/src/Shape/TestShape.java
+++ Ch5 Polymorphism/src/Shape/TestShape.java
@@ -4,8 +4,12 @@ public class TestShape {
         Shape s1;   //สร้าง Ref ได้
         //Shape s2 = new Shape(); //เป็น Abstract New ไม่ได้
         Circle c1;
-        //Circle c2 = new Circle();   //ยังติด Abstract ทำให้ New ไม่ได้
-        Circle2 c3 = new Circle2();
+        Circle2 c2 = new Circle2(2.5,1);
+        double area=c2.findArea();
+        double circum=c2.findCircumference();
+        System.out.println("Area C2="+area);
+        System.out.println("Circum C2="+circum);
+        System.out.println(c2);
     }
 }
 
