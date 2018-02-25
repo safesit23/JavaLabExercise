220dc3a0c8981ca1be1d5581187014189d04a934
Jatawatsafe <jatawat.safe@mail.kmutt.ac.th>
2018-02-25 15:16:46 +0700

    [Upload] ExerciseInheritance(Employee)

---
+++ INT105/src/Inheritance/Employee.java
@@ -0,0 +1,46 @@
+package Inheritance;
+
+public class Employee {
+    private int employeeId;
+    private String name;
+    private double salary;
+
+    public Employee() {
+    }
+
+    public Employee(int employeeId, String name, double salary) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "employeeId=" + employeeId + ", name=" + name + ", salary=" + salary + '}';
+    }
+
+}
