b9f0b8f09b5456025e421bbc771ee32495373e2e
INT105 <INT105@10.4.21.111>
2018-02-26 09:02:26 +0700

    [Upload] Warmup (Medicine)

---
+++ Ch6 BeforeFinal/src/ch6/beforefinal/MedicAble.java
@@ -0,0 +1,6 @@
+package ch6.beforefinal;
+public interface MedicAble {
+    public abstract String medicPropertise();
+    public abstract String[] medicIngregients();
+    public abstract String medicWarning();
+}
