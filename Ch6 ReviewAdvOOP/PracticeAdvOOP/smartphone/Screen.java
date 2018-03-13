8fb25dbd459218606bdfa01bd35cf925d2bd90d9
Jatawatsafe <35445358+safesit23@users.noreply.github.com>
2018-03-13 20:28:34 +0700

    [Add] addItem Method
    ทำการเช็คขนาด grid ว่าเกินหรือไม่หากไม่เกินก็จะทำการเพิ่ม items ลงบนหน้าจอ

--- Ch6 ReviewAdvOOP/PracticeAdvOOP/smartphone/Screen.java
+++ Ch6 ReviewAdvOOP/PracticeAdvOOP/smartphone/Screen.java
@@ -8,7 +8,25 @@ public class Screen {
     private int gridCount;
 
     public Screen() {
-
+        this.items = new Item[MAX_GRID];
+        this.itemCount=0;
+        this.gridCount=0;
+    }
+    
+    public boolean addItem(Item obj){
+        int size =1;
+        if(obj !=null && obj instanceof Widget){
+            Widget wg= (Widget) obj;
+            size=wg.getnGrid();
+        }
+        if(this.gridCount+size<=24){
+            this.items[itemCount]=obj;
+            this.itemCount++;
+            this.gridCount=this.gridCount+size;
+            return true;
+        }else{
+            return false;
+        }
     }
 
     public Item[] getItems() {
