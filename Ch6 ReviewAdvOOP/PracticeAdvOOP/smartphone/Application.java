314cd1924a173e2386ac9dcd2fc6437bfd5b08f1
Jatawatsafe <35445358+safesit23@users.noreply.github.com>
2018-03-13 18:48:45 +0700

    [Add] Part 1 การเช็ค Equals
    ต้องเขียน method Equals เองเพื่อเช็คชื่อและ icon
    ไม่อย่างนั้นจะ run ผ่านเพียง 50% เนื่องจากติดปัญหา junit.framework.AssertionFailedError while doing assertEquals on String[]
    คือปัญหาที่ไม่สามารถเทียบว่าเท่ากันได้หรือไม่

--- Ch6 ReviewAdvOOP/PracticeAdvOOP/smartphone/Application.java
+++ Ch6 ReviewAdvOOP/PracticeAdvOOP/smartphone/Application.java
@@ -2,14 +2,36 @@ package smartphone;
 
 import java.util.Objects;
 
-public class Application extends Item {
+public class Application extends Item{
 
     private String icon;
 
     public Application(String name, String icon) {
-
+        super(name);
+        this.icon=icon;
     }
 
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+    
+    public boolean equals(Object obj){
+        Application temp=null;
+        if(obj !=null && obj instanceof Application){
+            temp = (Application) obj;
+            if(super.getName().equals(temp.getName())){
+                if(icon.equals(temp.getIcon())){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
     @Override
     public String toString() {
         return "Application: "+this.getName();
