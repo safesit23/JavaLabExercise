64ef4f95f116fae728c71b89f54fb35ec5c03079
INT105 <INT105@10.4.21.111>
2018-03-26 10:56:47 +0700

    [ADD] อีกวิํธีหนึ่งในการเก็บ items

--- Ch9 FileAndDB/src/sit/db/ConnectionManager.java
+++ Ch9 FileAndDB/src/sit/db/ConnectionManager.java
@@ -3,16 +3,27 @@ package sit.db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionManager {
+    //เก็บแยก items
     public static Connection createConnection 
-        (String url, String username, String psw) throws ClassNotFoundException, SQLException{
+        (String url, String username, String psw) 
+        throws ClassNotFoundException, SQLException{
             //Optional for register driver name to DriverManager
             Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
             
             //บอกที่อยู่ของ Database ตัวนั้นเพื่อ Connect
             Connection cn=DriverManager.getConnection(url, username, psw);
             return cn;
+    }
+    
+    //อีกวิธีหนึ่งเอา items ไปเก็บใน Properties
+    public static Connection createConnection(String url, Properties props) throws SQLException{
+        String username=props.getProperty("username");
+        String psw=props.getProperty("password");
+        Connection cn=DriverManager.getConnection(url, username, psw);
+        return cn;
     }
     
 }
