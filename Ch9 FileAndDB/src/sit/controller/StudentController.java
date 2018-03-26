85b810c3ab74b2c2bf4a2ac0a7937e3d45d34446
INT105 <INT105@10.4.21.111>
2018-03-26 11:04:02 +0700

    [Edit] Another ways to Connection by Properties

--- Ch9 FileAndDB/src/sit/controller/StudentController.java
+++ Ch9 FileAndDB/src/sit/controller/StudentController.java
@@ -1,6 +1,7 @@
 package sit.controller;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 import sit.db.ConnectionManager;
 
 public class StudentController {
@@ -9,9 +10,16 @@ public class StudentController {
     public StudentController(String username, String psw) 
             throws ClassNotFoundException, SQLException{ //Constructor
         
-        String url="jdbc:derby://localhost:1527/student";
+        String url="jdbc:derby://localhost:1527/student";           
         con=ConnectionManager.createConnection(url, username, psw);
         System.out.println("Connection created sucessfully");
         
+    }
+    
+    //Overloading Constructor
+    public StudentController(Properties pro) throws SQLException{
+        String url="jdbc:derby://localhost:1527/student";           
+        con=ConnectionManager.createConnection(url, pro);
+        System.out.println("Connection created sucessfully");
     }
 }
