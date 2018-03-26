2654f9a01a5857675463d916c5a48d0f85a42b8a
INT105 <INT105@10.4.21.111>
2018-03-26 09:20:08 +0700

    [Add] try-catch + Scanner from file

--- Ch9 FileAndDB/src/ch9/fileanddb/TestScanner.java
+++ Ch9 FileAndDB/src/ch9/fileanddb/TestScanner.java
@@ -1,13 +1,29 @@
 package ch9.fileanddb;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class TestScanner {
     public static void main(String[] args) {
-        String myMessage="Mango    Grape   Apple   Guana";
-        Scanner sc = new Scanner(myMessage);
-        while(sc.hasNext()){    //ยังมีคำให้อ่านไหม
-            System.out.println(sc.next());  //สนใจเป็นคำ
+        try{
+            File myFile=new File("StudentList.txt");
+            Scanner sc = new Scanner(myFile);
+            while(sc.hasNextLine()){   
+                String line=sc.nextLine();
+                System.out.println(line);
+                
+                Scanner scStr=new Scanner(line);
+                while(scStr.hasNext()){
+                    System.out.println(scStr.next());
+                }
+                System.out.println("----");
+            }
         }
+        catch(FileNotFoundException ff){
+            System.out.println(ff);
+        }
+
+        
     }
 }
