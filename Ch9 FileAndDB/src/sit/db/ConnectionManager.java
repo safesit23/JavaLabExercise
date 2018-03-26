1766dda11d85df632adfeb52a86946c2c72ee3a5
INT105 <INT105@10.4.21.111>
2018-03-26 10:27:39 +0700

    [Upload] ConnectionManager

---
+++ Ch9 FileAndDB/src/sit/db/ConnectionManager.java
@@ -0,0 +1,18 @@
+package sit.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+    public static Connection createConnection 
+        (String url, String username, String psw) throws ClassNotFoundException, SQLException{
+            //Optional for register driver name to DriverManager
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+            
+            //บอกที่อยู่ของ Database ตัวนั้นเพื่อ Connect
+            Connection cn=DriverManager.getConnection(url, username, psw);
+            return cn;
+    }
+    
+}
