ee75b7233bacfc25c17a563758111f3b5e5837da
INT105 <INT105@10.4.21.111>
2018-03-26 09:24:47 +0700

    [Add] Comment loop

--- Ch9 FileAndDB/src/ch9/fileanddb/TestScanner.java
+++ Ch9 FileAndDB/src/ch9/fileanddb/TestScanner.java
@@ -6,15 +6,18 @@ import java.util.Scanner;
 
 public class TestScanner {
     public static void main(String[] args) {
+        int numOfLine=0,numOfWord=0;
         try{
             File myFile=new File("StudentList.txt");
             Scanner sc = new Scanner(myFile);
-            while(sc.hasNextLine()){   
+            while(sc.hasNextLine()){   //Loop นับบรรทัด
+                numOfLine+=1;
                 String line=sc.nextLine();
                 System.out.println(line);
                 
                 Scanner scStr=new Scanner(line);
-                while(scStr.hasNext()){
+                while(scStr.hasNext()){         //Loop นับคำ
+                    numOfWord+=1;
                     System.out.println(scStr.next());
                 }
                 System.out.println("----");
@@ -23,7 +26,8 @@ public class TestScanner {
         catch(FileNotFoundException ff){
             System.out.println(ff);
         }
+        System.out.println("Number of Lines: "+numOfLine);
+        System.out.println("Number of Words: "+numOfWord);
 
-        
     }
 }
