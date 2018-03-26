16bef1a87f74126cb329c978e7f7222fc8261f53
INT105 <INT105@10.4.21.111>
2018-03-26 11:31:42 +0700

    [Add] Execution and Close Connection Student

--- Ch9 FileAndDB/src/sit/controller/StudentController.java
+++ Ch9 FileAndDB/src/sit/controller/StudentController.java
@@ -1,10 +1,11 @@
 package sit.controller;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 import sit.db.ConnectionManager;
 
-public class StudentController {
+public class StudentController { //จัดการเฉพาะ Student DB
     private Connection con;
     
     public StudentController(String username, String psw) 
@@ -16,10 +17,25 @@ public class StudentController {
         
     }
     
-    //Overloading Constructor
+    ////Another ways to Connection by Properties (Overloading Constructor)
     public StudentController(Properties pro) throws SQLException{
         String url="jdbc:derby://localhost:1527/student";           
         con=ConnectionManager.createConnection(url, pro);
         System.out.println("Connection created sucessfully");
     }
+    
+    //Execution
+    public int deleteAllStudents() throws SQLException{
+        int deletedRecs=0;
+        String sql="delete from student";
+        Statement stmt=con.createStatement();
+        deletedRecs=stmt.executeUpdate(sql);
+        return deletedRecs;
+    }
+    
+    //Close Connection of Student DB
+    public void close() throws SQLException{
+        ConnectionManager.closeConnection(con);
+    }
+    
 }
