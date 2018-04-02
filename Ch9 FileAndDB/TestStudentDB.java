ddedfc0264990c2543ca58e0d289802597ca3121
Jatawatsafe <35445358+safesit23@users.noreply.github.com>
2018-04-02 09:53:17 +0700

    [Add] Connection by Properties by File

--- Ch9 FileAndDB/TestStudentDB.java
+++ Ch9 FileAndDB/TestStudentDB.java
@@ -1,28 +1,73 @@
 package sit.view;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Properties;
 import sit.controller.StudentController;
 import sit.model.Student;
 
 public class TestStudentDB {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        int rec;
         try{
+            //Connection1
             //StudentController stdCtrl = new StudentController("sit","sit");
+            
+            //Connection by Properties
+            /*
             Properties dbPros=new Properties();
             dbPros.setProperty("user", "sit");
             dbPros.setProperty("password", "sit");
             StudentController stdCtrl = new StudentController(dbPros);
+            */
+            
+            //Connection by Properties by File
+            String file="dpProperties.txt";
+            StudentController stdCtrl = new StudentController(file);
             
             //deleteAllStudents
             /*
-            int recs=stdCtrl.deleteAllStudents();
+            rec=stdCtrl.deleteAllStudents();
             System.out.println("deleted"+recs+" records");
             */
             
             //inserted
-            int rec=stdCtrl.insertStudent(new Student(99,"James","bonds"));
+            /*
+            rec=stdCtrl.insertStudent(new Student(99,"James","bonds"));
             System.out.println("inserted "+rec+" record");
+            */
+            
+            //findStudentById
+            //stdCtrl.findStudentById(201);
+            
+            //findStudentByname
+            //stdCtrl.findStudentByname("James");
+            
+            //updateStudent
+            /*
+            rec=stdCtrl.updateStudent(new Student(99,"James","bonds"));
+            System.out.println("inserted "+rec+" record");
+            */
+            
+            //insertStudentBatch(File)
+            /*
+            File myFile=new File("StudentList.txt");
+            rec=stdCtrl.insertStudentBatch(myFile);
+            System.out.println("inserted "+rec+" record");
+            */
+            
+            //insertStudentBatch(ArrayList)
+            /*ArrayList<Student> stdList = new ArrayList<Student>();
+            stdList.add(new Student(201,"AA","aaa"));
+            stdList.add(new Student(202,"BB","bbb"));
+            stdList.add(new Student(203,"CC","ccc"));
+            System.out.println(""+stdList.size());
+            rec=stdCtrl.insertStudentBatch(stdList);
+            System.out.println("inserted "+rec+" record");
+            */
             
         }
         /*
