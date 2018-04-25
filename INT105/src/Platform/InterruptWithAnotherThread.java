74fa7672193f363c8f8016a9feb2c7305bcae5c8
Jatawatsafe <35445358+safesit23@users.noreply.github.com>
2018-04-25 10:48:50 +0700

    [Upload] InterruptWithAnotherThread

---
+++ INT105/src/Platform/InterruptWithAnotherThread.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class InterruptWithAnotherThread{
+    //print name of thread
+    static void threadMessage(String message) {
+        String threadName =
+            Thread.currentThread().getName();
+        System.out.format("%s: %s%n",
+                          threadName,
+                          message);
+    }
+
+    private static class MessageLoop
+        implements Runnable {
+        public void run() {
+            try {
+                int i=0;
+                while(true){
+                    threadMessage(""+i);
+                    i++;
+                    Thread.sleep(1000);
+                }
+               
+            } catch (InterruptedException e) {
+                threadMessage("OKAY !");
+            }
+        }
+    }
+
+
+    public static void main(String args[]) throws InterruptedException {
+        threadMessage("-------------START-------------");
+        long startTime = System.currentTimeMillis();
+        Thread t = new Thread(new MessageLoop());   //Object t
+        t.start(); //เริ่มการทำงาน
+        Scanner input = new Scanner(System.in);
+        input.nextLine(); //รอการพิมพ์เพื่อรันต่อ
+        t.interrupt();    //Interrupt t
+        t.join();
+        threadMessage("BYE BYE!");
+    }
+}
