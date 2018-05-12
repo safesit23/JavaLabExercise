c6ca6c07f91eb0e9339b700b0b1b67c9cb3f237c
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-05-12 10:01:08 +0700

    [Upload] Exception Ex1 NotANumber

--- MacProINT105/src/TestException.java
+++ MacProINT105/src/TestException.java
@@ -1,7 +1,5 @@
 
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class TestException {
     public static void handlerException(String message) throws NotANumberException{
