07aa0aeecd41a42689bb35cd942be75ed7367509
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-05-17 12:42:38 +0700

    [Add] Another basic component

--- MacProINT105/src/GUIComponent/PanelAndCompTest.java
+++ MacProINT105/src/GUIComponent/PanelAndCompTest.java
@@ -1,17 +1,25 @@
 package GUIComponent;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class PanelAndCompTest {
     public static void main(String[] args) {
+        //--------Container--------
         JFrame frame = new JFrame("Hello World");
         JPanel panel = new JPanel();
-        
+        //--------Component---------
         JLabel label = new JLabel("Hello World");
-        
+        JButton button = new JButton("CLICK BUTTON");
+        JTextField txtfield = new JTextField("Default Message",20);
+        //-----panel add component
         panel.add(label);
+        panel.add(button);
+        panel.add(txtfield);
+        //------ frame add panel
         frame.add(panel);
         
         frame.setSize(400,400);
