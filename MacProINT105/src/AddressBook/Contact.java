1cdc706dc5e3ba60717b6bf2c32f623345e0f4e3
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-05-24 22:56:35 +0700

    [Upload] Contact

---
+++ MacProINT105/src/AddressBook/Contact.java
@@ -0,0 +1,55 @@
+
+package AddressBook;
+
+public class Contact {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String phone;
+
+    public Contact(int id, String firstName, String lastName, String phone) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" + "id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", phone=" + phone + '}';
+    }
+    
+    
+}
