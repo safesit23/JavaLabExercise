a603e03e3ab4c3529c0b4b7eea8fbe66cdd63870
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-05-25 00:37:35 +0700

    [Add] set text in SearchBtn

--- MacProINT105/src/AddressBook/SearchButtonListener.java
+++ MacProINT105/src/AddressBook/SearchButtonListener.java
@@ -8,36 +8,47 @@ import javax.swing.JTextField;
 
 public class SearchButtonListener implements ActionListener{
     private  JTextField textSearch;
-    private JButton buttonSearch;
     private JTextField textId;
     private JTextField textFirstName;
     private JTextField textLastName;
     private JTextField textPhone;
     private JButton buttonLeft;
     private JButton buttonRight;
+    private AddressBookController addBook;
 
-    public SearchButtonListener(JTextField textSearch, JTextField textId, JTextField textFirstName, JTextField textLastName, JTextField textPhone, JButton buttonLeft, JButton buttonRight) {
+    public SearchButtonListener(JTextField textSearch, JTextField textId, JTextField textFirstName, JTextField textLastName, JTextField textPhone, JButton buttonLeft, JButton buttonRight) throws SQLException {
         this.textSearch = textSearch;
-        this.buttonSearch = buttonSearch;
         this.textId = textId;
         this.textFirstName = textFirstName;
         this.textLastName = textLastName;
         this.textPhone = textPhone;
         this.buttonLeft = buttonLeft;
         this.buttonRight = buttonRight;
+        this.addBook = new AddressBookController();
     }
     
     @Override
     public void actionPerformed(ActionEvent e) {
         try{
-            AddressBookController addBook = new AddressBookController();
-            
+            System.out.println(e.getActionCommand());
             if(e.getActionCommand().equals("Search")){
+                textId.setText("");
+                textFirstName.setText("");
+                textLastName.setText("");
+                textPhone.setText(" ");
                 Contact contact = addBook.searchData(textSearch.getText());
+                textId.setText(Integer.toString(contact.getId()));
+                textFirstName.setText(contact.getFirstName());
+                textLastName.setText(contact.getLastName());
+                textPhone.setText(contact.getPhone());
                 buttonLeft.setVisible(false);
                 buttonRight.setVisible(true);
             }else if(e.getActionCommand().equals("<")){
                 Contact contact = addBook.leftIndex();
+                textId.setText(Integer.toString(contact.getId()));
+                textFirstName.setText(contact.getFirstName());
+                textLastName.setText(contact.getLastName());
+                textPhone.setText(contact.getPhone());
                 if(addBook.getIndex()==0){
                     buttonLeft.setVisible(false);
                     buttonRight.setVisible(true);
@@ -47,6 +58,10 @@ public class SearchButtonListener implements ActionListener{
                 }
             }else if(e.getActionCommand().equals(">")){
                 Contact contact = addBook.rightIndex();
+                textId.setText(Integer.toString(contact.getId()));
+                textFirstName.setText(contact.getFirstName());
+                textLastName.setText(contact.getLastName());
+                textPhone.setText(contact.getPhone());
                 if(addBook.getIndex()==addBook.getArrSize()){
                     buttonLeft.setVisible(true);
                     buttonRight.setVisible(false);
